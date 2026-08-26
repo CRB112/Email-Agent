ff2f3ad65fa15f3615d221ab0029bd580c5584ad
Connor <connorrb1111@gmail.com>
2026-08-26 13:16:42 -0400

    Added ability to ove emails

--- app/parser/ModifyCommands.py
+++ app/parser/ModifyCommands.py
@@ -1,3 +1,8 @@
+from msgraph.generated.users.item.messages.item.move.move_post_request_body import (
+    MovePostRequestBody,
+)
+
+
 class Modify:
     def __init__(self, settings : dict):
         if settings is None:
@@ -11,8 +16,23 @@ class Delete(Modify):
     def __init__(self, settings : dict):
         super().__init__(settings)
     async def modify(self, email, g_client):
-        await g_client.me.messages.by_message_id(email.id).delete() 
+        return await g_client.me.messages.by_message_id(email.id).delete() 
+
+class Move(Modify):
+    def __init__(self, settings : dict):
+        super().__init__(settings)
+        self.dest = settings["Folder"]
+    async def modify(self, email, g_client):
+        request_body = MovePostRequestBody(destination_id=self.dest)
+        
+        return await (
+            g_client.me.messages
+            .by_message_id(email.id)
+            .move
+            .post(request_body)
+        )
 
 MODIFY_CLASSES = {
-    "Delete" : Delete
+    "Delete" : Delete,
+    "Move" : Move
 }
